d03b5c77b0a2854cda7dcabd33a488a1fe9f77f8
Bill Chung <bill_chungiii@hotmail.com>
2020-01-19 11:37:56 +0800

    加入專案檔案。

---
+++ GetDiskFreeSpace/DiskSpaceHelper.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GetDiskFreeSpace
+{
+    class DiskSpaceHelper
+    {
+        [DllImport("kernel32.dll", SetLastError = true, CharSet = CharSet.Auto)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool GetDiskFreeSpaceEx(string lpDirectoryName,out ulong lpFreeBytesAvailable,out ulong lpTotalNumberOfBytes,out ulong lpTotalNumberOfFreeBytes);
+
+
+
+        public static (string directoryName,string totalSpace, string freeSpace, double freePrecent ) GetDiskSpaceInfo(string directoryName)
+        {
+            GetDiskFreeSpaceEx(directoryName, out ulong available, out ulong total, out ulong free);
+
+
+
+            return (directoryName, $"{ConvertToMB(total)} MB ({total})", $"{ConvertToMB(available)} MB ({available})", ((double)available / total ) );
+        }
+
+        private static double ConvertToMB(ulong value)
+        {
+            return value / 1048576.0;
+        }
+    }
+}
---
+++ GetDiskFreeSpace/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GetDiskFreeSpace
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var result = DiskSpaceHelper.GetDiskSpaceInfo(@"C:\");
+            Console.WriteLine(result.directoryName);
+            Console.WriteLine(result.totalSpace);
+            Console.WriteLine(result.freeSpace);
+            Console.WriteLine(result.freePrecent);
+
+
+            Console.ReadLine();
+        }
+    }
+}
